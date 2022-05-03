d99138e70e86f00c6747826b0d8f420916140488
Matt Bellis <matthew.bellis@gmail.com>
2022-05-03 10:08:23 -0400

    Some edits

--- pet_scanner/pet_scanner.ino
+++ pet_scanner/pet_scanner.ino
@@ -1,20 +1,4 @@
-/*
-int led1 = 1;
-int led2 = 2;
-int led3 = 3;
-int led4 = 4;
-int led5 = 5;
-int led6 = 6;
-int led7 = 7;
-int led8 = 8;
-int led9 = 9;
-int led10 =10;
-int led11 = 11;
-int led12 = 12;
-*/
-
-
-int PET_radius = 150;
+int PET_radius = 100;
 int PET_diameter;
 int center_x = 0;
 int center_y = 0;
@@ -22,7 +6,15 @@ int center_y = 0;
 // Tumor location
 float theta_tumor;
 float radial_tumor;
-float x_tumor, y_tumor;
+
+///////////////////////////////////////////
+// Instructor can change these
+///////////////////////////////////////////
+float x_tumor = -25;
+float y_tumor = -60;
+
+int delay_time = 100;
+///////////////////////////////////////////
 
 int nleds = 12;
 int i = 0;
@@ -30,7 +22,7 @@ int i = 0;
 long MAX_RAND = 1000000;
 float rnd;
 
-int led[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
+int led[] = {2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13};
 
 // the setup routine runs once when you press reset:
 void setup() {
@@ -51,17 +43,15 @@ void setup() {
     digitalWrite(led[i],LOW);
   }
 
- PET_diameter = 2*PET_radius;
+   PET_diameter = 2*PET_radius;
 
   // Location of the tumor
-  //float x = random(0,MAX_RAND)/float(MAX_RAND);
-  //Serial.println(x);
-  //Serial.println(MAX_RAND);
-
+  // Uncomment the section below if we want the tumor
+  // to be placed at a random location
+  /*
   rnd = random(0,MAX_RAND)/float(MAX_RAND);
   //Serial.println(rnd);
-  theta_tumor = acos((2*rnd)-1);
-  
+  theta_tumor = acos((2*rnd)-1);  
   rnd = random(0,MAX_RAND)/float(MAX_RAND);
   //Serial.println(rnd);
   radial_tumor =((2*PET_radius*rnd)-PET_radius);
@@ -69,18 +59,21 @@ void setup() {
   x_tumor = radial_tumor*cos(theta_tumor);
   y_tumor = radial_tumor*sin(theta_tumor);
 
-  x_tumor = 75;
-  y_tumor = 75;
+  //x_tumor = 50;
+  //y_tumor = -50;
+  */
   
   Serial.println("Location of tumor\n");
-  Serial.println(theta_tumor);
-  Serial.println(radial_tumor);
+  //Serial.println(theta_tumor);
+  //Serial.println(radial_tumor);
   Serial.println(x_tumor);
   Serial.println(y_tumor);
   
   Serial.println("Setup finished!");
   
 }
+
+//////////////////////////////////////////////////////////////
 void loop() {
  // put your main code here, to run repeatedly:
  // the loop routine runs over and over again forever:
@@ -117,8 +110,8 @@ void loop() {
 
   points_of_intersection(PET_radius,m,b, xintercept, yintercept);
 
+  /*
   Serial.println("------");
-
   Serial.println("Location of tumor\n");
   Serial.println(theta_tumor);
   Serial.println(radial_tumor);
@@ -131,45 +124,48 @@ void loop() {
 
   Serial.println(xintercept[1]);
   Serial.println(yintercept[1]);
-
+  */
+  
   float r1 = xintercept[0]*xintercept[0] + yintercept[0]*yintercept[0];
   float r2 = xintercept[1]*xintercept[1] + yintercept[1]*yintercept[1];
-  Serial.println(r1);
-  Serial.println(r2);
+  
+  //Serial.println(r1);
+  //Serial.println(r2);
 
-  Serial.println("------");
+  //Serial.println("------");
 
   // Which one of the lights do they light up!!!!!!!!!
   float angle_of_detector1 = atan2(yintercept[0],xintercept[0]);
   float angle_of_detector2 = atan2(yintercept[1],xintercept[1]);
 
-  Serial.println(angle_of_detector1);
-  Serial.println(angle_of_detector2);
+  //Serial.println(angle_of_detector1);
+  //Serial.println(angle_of_detector2);
 
   float detector_arc = 2*PI/nleds;
 
   int led_hit1 =  which_detector(angle_of_detector1, nleds, detector_arc);
   int led_hit2 =  which_detector(angle_of_detector2, nleds, detector_arc);
 
+  /*
   Serial.println("LEDs to turn on");
   Serial.println(led_hit1);
   Serial.println(led_hit2);
   Serial.println(led[led_hit1]);
   Serial.println(led[led_hit2]);
- 
+  */
 
   digitalWrite(led[led_hit1], HIGH);
   digitalWrite(led[led_hit2], HIGH);
     
-  delay(2000);
+  delay(delay_time);
 
   digitalWrite(led[led_hit1], LOW);
   digitalWrite(led[led_hit2], LOW);
 
   // Turn them all off
-  Serial.println("Turn them all off!");
+  //Serial.println("Turn them all off!");
  for (int i=0;i<nleds;i++) {
-    Serial.println(i);
+    //Serial.println(i);
     digitalWrite(led[i],LOW);
   }
   
@@ -181,10 +177,10 @@ void loop() {
 
 void quadratic(float a, float b, float c, float n[2]) {
 
-  Serial.println("In quadratic!!!!\n");
+  //Serial.println("In quadratic!!!!\n");
   
   float radicand = b*b - (4*a*c);
-  Serial.println(radicand);
+  //Serial.println(radicand);
   float n1 = -999;
   float n2 = -999;
   if ( radicand < 0 ) {
@@ -232,18 +228,18 @@ void points_of_intersection(float r, float m, float b, float x[2], float y[2]){
 
 int which_detector(float theta, int narcs, float arc_width){
 
-   Serial.println("In which detector...");
+   //Serial.println("In which detector...");
    int detector = 0;
    for (i=0;i<narcs;i++) {
-    Serial.println(i);
-    Serial.println(theta);
-    Serial.println(theta+PI);
-    Serial.println(i*arc_width);
-    Serial.println((i+1)*arc_width);
+    //Serial.println(i);
+    //Serial.println(theta);
+    //Serial.println(theta+PI);
+    //Serial.println(i*arc_width);
+    //Serial.println((i+1)*arc_width);
     if (theta+PI >= i*arc_width && theta+PI < (i+1)*arc_width) {
       detector = i;
       break;
-   }
+    }
    }
 
    return detector;
